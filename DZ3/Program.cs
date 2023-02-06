a34706c021eed69a469bdd552980fe202be9af54
Ilya <jyg3a125@gmail.com>
2023-02-06 15:38:54 +0500

    add DZ3

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -26,18 +26,16 @@ int GetNumber(string message)
 
 int x1 = GetNumber("Введите координату Х первой точки");
 int y1 = GetNumber("Введите координату Y первой точки");
+int z1 = GetNumber("Введите координату Z первой точки");
 
 int x2 = GetNumber("Введите координату Х второй точки");
 int y2 = GetNumber("Введите координату Y второй точки");
+int z2 = GetNumber("Введите координату Z второй точки");
 
-// // Корень((x1-x2)^2 + (y1-y2)^2)
-// // Math.Sqrt()
-// // Math.Pow(выражение, степень)
-
-double GetDistance (int x1, int x2, int y1, int y2)
+double GetDistance (int x1, int x2, int y1, int y2, int z1, int z2)
 {
-    double result = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2));
+    double result = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2)+ Math.Pow(z1-z2,2));
     return result;
 } 
 
-System.Console.WriteLine(GetDistance(x1,x2,y1,y2));
+Console.WriteLine(GetDistance(x1,x2,y1,y2,z1,z2));
